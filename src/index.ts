65360730078edfdc2f44094bcced7b0f525cffa0
rohit-pahuja <rohitpahuja149.rp@gmail.com>
2025-05-30 00:48:52 +0530

    Add morgan middleware for logging HTTP requests.

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import { connectDB } from './config/dbConnection';
 import userRoutes from './routes/userRoutes';
 import todoRoutes from './routes/todoRoutes';
 import { startCronJob } from './cronScheduler';
+import morgan from 'morgan';
 import dotenv from 'dotenv';
 dotenv.config();
 
@@ -10,6 +11,7 @@ const app = express();
 const PORT = process.env.PORT || 4000;
 
 app.use(express.json());
+app.use(morgan('dev')); 
 
 app.use('/user', userRoutes);
 app.use('/todo', todoRoutes);
